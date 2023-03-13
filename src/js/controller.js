36d8f43a043809b3aee8d6bfa2592bd4f2cfd2c2
MahdiCoder <mahdi202201.mohammadi@gmail.com>
2023-03-13 16:22:12 +0430

    fetching respone with recipe api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,26 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+    );
+    const data = await res.json();
+    console.log(res);
+    console.log(data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+    };
+    console.log(recipe);
+    if (!res.ok) throw new Error(`${data.message} ${res.status} `);
+  } catch (error) {
+    console.error(`${error.message} 😫`);
+  }
+};
+showRecipe();
